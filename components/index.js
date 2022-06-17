c96c06da80742218a24ab703d9e253a6982e595c
Ali Adruich <52458295+adruich-ali@users.noreply.github.com>
2022-06-17 16:35:17 +0100

    First screen cREATION

---
+++ components/index.js
@@ -0,0 +1,10 @@
+import { CircleButton, RectButton  } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { SubInfo } from "./SubInfo";
+
+
+export {HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton ,SubInfo, DetailsBid, DetailsDesc}
---
+++ screens/Details.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text, View,Image, StatusBar, FlatList } from 'react-native';
+import {COLORS, SIZES,SHADOWS,FONTS,assets} from '../constants'
+import {CircleButton, RectButton, SubInfo, FocusedStatusBar, DetailsDesc, DetailsBid} from '../components'
+const Details = ({route, navigation}) => {
+  const {data} = route.
+  return (
+    <Text>Details</Text>
+  )
+}
+
+export default Details
